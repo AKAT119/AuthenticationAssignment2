c4678b1d391af5afe1f3f970d65a30bc8870d661
Azin Katiraee <azinnash@yahoo.com>
2022-10-29 10:29:57 -0400

    updated db.js & list.ejs

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = 
 {
     
-    "URI": "mongodb://127.0.0.1:27017/book_store"
+   // "URI": "mongodb://127.0.0.1:27017/book_store"
+    "URI": "mongodb+srv://Azinnash:nash221n@cluster0.xk6ifkg.mongodb.net/book_store?retryWrites=true&w=majority"
 }
