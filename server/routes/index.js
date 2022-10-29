0f105d6085b373fb53ba625ea498bfddc7b74db5
Azin Katiraee <azinnash@yahoo.com>
2022-10-29 00:37:29 -0400

    updatinf files in view and model

--- server/routes/index.js
+++ server/routes/index.js
@@ -34,6 +34,20 @@ router.get('/services', function(req, res, next) {
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Books'});
 });
+/* GET Route for displaying the Login page */
+router.get('/login', indexController.displayLoginPage);
+
+/* POST Route for processing the Login page */
+router.post('/login', indexController.processLoginPage);
+
+/* GET Route for displaying the Register page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* POST Route for processing the Register page */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET to perform UserLogout */
+router.get('/logout', indexController.performLogout);
 
 
 
